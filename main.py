a1d397eda05220da2acd4c42963f5230ab9eb580
Ben Landon <81256789+https123456789@users.noreply.github.com>
2021-11-01 19:00:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ currentDate = datetime.datetime.now()
 date = currentDate.strftime("%m-%d-%Y %I:%M")
 readmeData += "# Last Time Run\n" + str(date)
 
-drawing = svgwrite.Drawing('test.svg', profile='tiny')
-drawing.add(drawing.line((0, 0), (10, 10), stroke=svgwrite.rgb(10, 10, 16, "%"))
-drawing.save()
+dwg = svgwrite.Drawing('test.svg', profile='tiny')
+dwg.add(dwg.line((0, 0), (10, 0), stroke=svgwrite.rgb(10, 10, 16, '%')))
+dwg.add(dwg.text('Test', insert=(0, 0.2), fill='red'))
+dwg.save()
 
 # Write to README.md
 readmeFile = open("README.md", "w")
