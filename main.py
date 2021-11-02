b4f03736f8bd29fc6315541b64416660c679b130
Ben Landon <81256789+https123456789@users.noreply.github.com>
2021-11-02 17:04:27 +0000

    Generates for all repos now.

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys, os
-
-import datetime
+import sys, os, datetime, requests, json
 
 readmeData = """
 # Repo Stats
@@ -14,7 +12,34 @@ currentDate = datetime.datetime.now()
 date = currentDate.strftime("%m-%d-%Y %I:%M")
 readmeData += "# Last Time Run\n" + str(date)
 
-os.system("python generate.py " + sys.argv[1])
+# Get all repo names
+
+repos = []
+repoNames = []
+
+r1 = requests.get("https://api.github.com/users/https123456789/repos")
+r1.raise_for_status()
+reposFile = open("repos.json", "wb")
+#json.dump(r1.json, reposFile, indent=4)
+reposFile.write(r1.content)
+reposFile.close()
+reposFile = open("repos.json", "r")
+data = json.load(reposFile)
+reposFile.close()
+
+repos = data
+for repo in repos:
+	print(repo["full_name"])
+	repoNames.append(repo["full_name"])
+
+print(os.system("pwd"))
+
+for repo in repoNames:
+	dirName = "docs/" + repo.replace("https123456789/", "")
+	os.makedirs(dirName, exist_ok=True)
+	os.system("python generate.py " + repo.replace("https123456789/", ""))
+
+#os.system("python generate.py " + sys.argv[1])
 
 # Write to README.md
 readmeFile = open("README.md", "w")
